7bbd8cf5e5a5a2c25ac5eff7cabdcbb95a6ee267
etan Memeignton <eja9979@g.rit.edu>
2021-12-07 00:27:35 -0500

    2-2 done

--- problem_2/problem_2_1.py
+++ problem_2/problem_2_1.py
@@ -3,21 +3,40 @@
 
 def dive(filename):
     horizontal = 0
-    vertical = 0
+    depth = 0
     with open(filename) as file:
         for line in file:
             splt = line.lower().split()    
             if splt[0] == 'up':
-                vertical -= int(splt[1]) # Takes split line and sees what operation it must do
+                depth -= int(splt[1]) # Takes split line and sees what operation it must do
             elif splt[0] == 'down':
-                vertical += int(splt[1])            
-            elif splt[0].lower() == 'forward':
+                depth += int(splt[1])            
+            elif splt[0] == 'forward':
                 horizontal += int(splt[1])
     
-    return horizontal * vertical
+    return horizontal * depth
+
+# --------part 2-------------
+
+def dive_accurate(filename, aim=0):
+    horizontal = 0
+    depth = 0
+    with open(filename) as file:
+        for line in file:
+            splt = line.lower().split()    
+            if splt[0] == 'up':
+                aim -= int(splt[1]) # Takes split line and sees what operation it must do
+            elif splt[0] == 'down': # Instead of adding to vertical 
+                aim += int(splt[1])  
+            elif splt[0] == 'forward':
+                horizontal += int(splt[1])
+                depth += aim * int(splt[1])       
+            
+    return horizontal * depth
 
 def main():
     print(dive("puzzle_2.txt"))
+    print(dive_accurate("puzzle_2.txt"))
 
 if __name__ == "__main__":
     main()
